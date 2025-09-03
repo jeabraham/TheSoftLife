a366174d5e72f632a92ed3b610388316e732e9db
John Abraham <jea@hbaspecto.com>
2025-09-03 15:05:27 -0600

    Initial Commit

---
+++ TheSoftLife/TheSoftLifeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TheSoftLifeApp.swift
+//  TheSoftLife
+//
+//  Created by John Abraham on 2025-09-03.
+//
+
+import SwiftUI
+
+@main
+struct TheSoftLifeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
